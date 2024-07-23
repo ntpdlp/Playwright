f4af382d26c401bc5b6222c19f13ef982118597e
ntpdlp <30646772+ntpdlp@users.noreply.github.com>
2024-07-23 13:47:36 -0700

    s08 init

---
+++ tests/s08_UI_getBy.spec.js
@@ -0,0 +1,20 @@
+
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ */
+
+import { test , expect} from '@playwright/test';
+
+//selector
+
+
+
+//testData
+const url = 'https://rahulshettyacademy.com/angularpractice/';
+
+test('UI Control', async ({ page }) => {
+
+    await page.goto(url);
+
+
+});
